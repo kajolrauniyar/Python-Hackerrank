e7575c6a9e0cffe0f5c9e386c88ce9d833e377fb
kajol <36220862+kajolrauniyar@users.noreply.github.com>
2020-05-04 21:00:31 +0530

    Create print_function.py

---
+++ Introduction/print_function.py
@@ -0,0 +1,31 @@
+Check Tutorial tab to know how to to solve.
+
+Read an integer .
+
+Without using any string methods, try to print the following:
+
+
+Note that "" represents the values in between.
+
+Input Format
+
+The first line contains an integer .
+
+Output Format
+
+Output the answer as explained in the task.
+
+Sample Input 0
+
+3
+Sample Output 0
+
+123
+
+
+
+
+from __future__ import print_function
+n = int(raw_input())
+for i in xrange(n):
+    print (i+1,end="")
